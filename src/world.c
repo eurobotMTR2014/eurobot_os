9f027201779f3fe729f29efdc312d7ecf9a2883f
Waliens <romainmormont@hotmail.com>
2014-03-22 16:18:48 +0100

    Implement functions for accessing the goal (synchronized) buffer

--- src/world.c
+++ src/world.c
@@ -46,8 +46,6 @@ typedef struct World_t {
 } World;
 
 // semaphores
-
-
 // static functions 
 /**
  * Initialize the fields of each encoder structure of the World
@@ -64,8 +62,13 @@ static void init_state();
  */
 static void init_goals_buffer();
 
+/**
+ * Resets counting semaphores og goal buffer
+ */
+void reset_goal_buffer_sem();
+
 // world data
-World world;
+volatile World world;
 
 
 void init_world()
@@ -73,37 +76,115 @@ void init_world()
 	init_encoders();
 	init_state();
 	init_goals_buffer();
-
 }
 
 static void init_encoders()
 {
 	// sets QEI peripheral 
-	world->prev_left.ulBase = QEI1_BASE;
-	world->curr_left.ulBase = QEI1_BASE;
-	world->prev_right.ulBase = QEI0_BASE;
-	world->curr_right.ulBase = QEI0_BASE;
+	world.prev_left.ulBase = QEI1_BASE;
+	world.curr_left.ulBase = QEI1_BASE;
+	world.prev_right.ulBase = QEI0_BASE;
+	world.curr_right.ulBase = QEI0_BASE;
 
 	// sets initial values for the fields
-	updateEncoder(&(world->prev_left));
-	updateEncoder(&(world->curr_left));
-	updateEncoder(&(world->prev_right));
-	updateEncoder(&(world->curr_right));
+	updateEncoder(&(world.prev_left));
+	updateEncoder(&(world.curr_left));
+	updateEncoder(&(world.prev_right));
+	updateEncoder(&(world.curr_right));
 }
 
 static void init_state()
 {
-	world->x = INIT_X_1;
-	world->y = INIT_Y_1;
-	world->stop = true;
+	world.x = INIT_X_1;
+	world.y = INIT_Y_1;
+	world.stop = true;
 }
 
 
 static void init_goals_buffer()
 {
-	world->goals_buffer->in = 0;
-	world->goals_buffer->out = 0;
-	world->goals_buffer->empty_slot_count = xSemaphoreCreateCounting(POSITION_GOAL_BUF_SIZE, POSITION_GOAL_BUF_SIZE);
-	world->goals_buffer->filled_slot_count = xSemaphoreCreateCounting(POSITION_GOAL_BUF_SIZE, 0);
-	world->goals_buffer->goals_mutex = xSemaphoreCreateBinary();
+	world.goals_buffer.in = 0;
+	world.goals_buffer.out = 0;
+	world.goals_buffer.goals_mutex = xSemaphoreCreateMutex();
+	reset_goal_buffer_sem();
+}
+
+static void reset_goal_buffer_sem()
+{
+	world.goals_buffer.empty_slot_count = xSemaphoreCreateCounting(POSITION_GOAL_BUF_SIZE, POSITION_GOAL_BUF_SIZE);
+	world.goals_buffer.filled_slot_count = xSemaphoreCreateCounting(POSITION_GOAL_BUF_SIZE, 0);
+}
+
+const PositionGoal* world_peek_next_goal()
+{
+	GoalsBuffer* gb = &(world.goals_buffer);
+	PositionGoal* pg;
+	
+	xSemaphoreTake(gb->filled_slot_count, portMAX_DELAY); // waits for data in the buffer
+	xSemaphoreTake(gb->goals_mutex); // mutex
+
+	pg = &(gb->goals[gb->out]);
+
+	xSemaphoreGive(gb->goals_mutex);
+
+	return pg;
+}
+
+PositionGoal* world_pick_next_goal()
+{
+	GoalsBuffer* gb = &(world.goals_buffer);
+	PositionGoal* pg;
+	
+	xSemaphoreTake(gb->filled_slot_count, portMAX_DELAY); // waits for data in the buffer
+	xSemaphoreTake(gb->goals_mutex); // mutex
+
+	pg = &(gb->goals[gb->out]);
+	gb->out = (gb->out + 1) % POSITION_GOAL_BUF_SIZE;
+
+	xSemaphoreGive(gb->goals_mutex);
+	xSemaphoreGive(gb->empty_slot_count);
+
+	return pg;
+}
+
+void world_put_goal(PositionGoal* pg)
+{
+	GoalsBuffer* gb = &(world.goals_buffer);
+
+	xSemaphoreTake(gb->empty_slot_count, portMAX_DELAY);
+	xSemaphoreTake(gb->goals_mutex);
+
+	gb->goals[gb->in] = *pg;
+	gb->in = (gb->in + 1) % POSITION_GOAL_BUF_SIZE;
+
+	xSemaphoreGive(gb->goals_mutex);
+	xSemaphoreGive(gb->filled_slot_count);
+}
+
+void world_goal_flush()
+{
+	GoalsBuffer* gb = &(world.goals_buffer);
+
+	xSemaphoreTake(gb->goals_mutex);
+	// reset semaphores 
+	xSemaphoreDelete(gb->filled_slot_count);
+	xSemaphoreDelete(gb->empty_slot_count);
+	reset_goal_buffer_sem();
+
+	gb->in = gb->out = 0;
+
+	xSemaphoreGive(gb->goals_mutex);
+}
+
+void world_goal_remove_peek()
+{
+	GoalsBuffer* gb = &(world.goals_buffer);
+
+	xSemaphoreTake(gb->filled_slot_count);
+	xSemaphoreTake(gb->goals_mutex);
+
+	gb->out = (gb->out + 1) % POSITION_GOAL_BUF_SIZE;
+
+	xSemaphoreGive(gb->goals_mutex);
+	xSemaphoreGive(gb->empty_slot_count);
 }
